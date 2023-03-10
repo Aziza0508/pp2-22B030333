cc901ef7311b2e9a06639fe3d09f44b57d603fd1
Aziza0508 <112758271+Aziza0508@users.noreply.github.com>
2023-03-10 08:20:50 +0600

    Update asumb.py

--- week 5/asumb.py
+++ week 5/asumb.py
@@ -1,9 +1,9 @@
 import re
 
 def task5(text):
-    pattern = 'a.?b$'
+    pattern = 'a.*b$'
     x = re.findall(pattern, text)
-    try:
-        print(x)
-    except AttributeError as e:
-        print("Not found")
+    print(x)
+    
+text = "ajcjkhb"
+task5(text)
