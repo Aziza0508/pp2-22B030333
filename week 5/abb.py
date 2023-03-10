d1f47fc8a3aa08c10128776bfd0488547d60e953
Aziza0508 <112758271+Aziza0508@users.noreply.github.com>
2023-03-10 08:13:37 +0600

    Update abb.py

--- week 5/abb.py
+++ week 5/abb.py
@@ -8,10 +8,7 @@ text2 = "aaab"
 def task2(text):
     pattern = 'ab{2,3}'
     x = re.findall(pattern, text)
-    try:
-        print(x)
-    except AttributeError as e:
-        print("Not found")
+    print(x)
 
 print(task2(text1))
 print(task2(text2))
